b2306128d88d6430fd8870fb3d3dda8dea600988
오우빈 <@DESKTOP-OJTHB5T>
2023-03-09 13:13:27 +0900

    초기 커밋.

---
+++ 01.SubjectSumPractice/SumPractice.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	int sum = 0;
+	int i = 2;
+	int j = 0;
+	int cnt = 0;
+
+	while (i < 101)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				cnt += 1;
+			}
+		}
+		if (cnt == 0)
+		{
+			sum += i;
+		}
+		cnt = 0;
+		i++;
+	}
+	printf("%d", sum);
+}
